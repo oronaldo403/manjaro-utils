ef32a9d252050ac5e43580e9a6a016b67dc7bbda
ronaldomachado403 <ronaldomachado403@gmail.com>
2024-11-20 11:41:18 -0300

    sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,6 @@
 ## Comandos de pós instalação do meu ambiente de desenvolvimento Manjaro
 
 sudo pacman -Syyu
-sudo pacman-mirrors --fasttrack
 sudo systemctl enable fstrim.timer
 sudo systemctl start fstrim.timer
 sudo pacman -S pavucontrol
@@ -9,7 +8,7 @@ sudo pacman -S ufw
 sudo pacman -S gufw
 sudo ufw enable
 sudo systemctl enable ufw
-sudo pacman -S base-devel jdk-openjdk openssl ctags ncurses ack the_silver_searcher fontconfig  libmagick imagemagick git gvim curl keepassxc neofetch htop gedit terminator gnome-tweaks gnome-browser-connector
+sudo pacman -S base-devel jdk-openjdk openssl ctags ncurses ack the_silver_searcher fontconfig  libmagick imagemagick git gvim curl keepassxc neofetch htop gedit terminator zsh yay
 sudo pacman -S docker
 sudo systemctl start docker
 sudo systemctl enable docker
